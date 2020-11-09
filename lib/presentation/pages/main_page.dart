7019b3bcca25fda2e318c8324b471e5f1326e836
jurkko <jure.turk@student.um.si>
2020-11-09 17:41:58 +0100

    some changes

--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:navigation_example/logic/get_books.dart';
 import 'package:navigation_example/models/book.dart';
 
@@ -8,10 +9,9 @@ class MainPage extends StatefulWidget {
 }
 
 class MainPageState extends State<MainPage> {
-  GetBooks getbooks = GetBooks();
-  List<Book> books = GetBooks.books;
-  int _index = 0;
 
+  int _index = 0;
+  List<Book> books = GetBooks.books;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,75 +19,62 @@ class MainPageState extends State<MainPage> {
         title: Text('Carousel'),
         backgroundColor: Colors.orange,
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Padding(
+    body: Container(
+      height:350,
+
+      child: PageView.builder(
+        controller: PageController(viewportFraction: 0.55),
+        scrollDirection: Axis.horizontal,
+        itemCount: books.length,
+        onPageChanged: (int index){
+          print(_index.toString());
+          setState(() {
+            _index = index;
+          });
+          print(_index.toString());
+        },
+        itemBuilder: (BuildContext context, int index){
+          Book book = books[index];
+          return Transform.scale(
+            scale: _index == index ? 1 : 0.9,
+            child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
-                height: 360,
-                child: PageView.builder(
-                  controller: PageController(viewportFraction: 0.55),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: books.length,
-                  onPageChanged: (int index) {
-                    print(_index.toString());
-                    setState(() => _index = index);
-                    print(_index.toString());
-                  },
-                  itemBuilder: (BuildContext context, int index) {
-                    Book book = books[index];
-                    return Transform.scale(
-                      scale: index == _index ? 1 : 0.9,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          child: Column(
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(10.0),
-                                child: Image(
-                                  height: 250.0,
-                                  width: 180.0,
-                                  image: AssetImage(book.image),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  '${book.name}',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: index == _index ? 15 : 12,
-                                    fontWeight: index == _index
-                                        ? FontWeight.bold
-                                        : FontWeight.normal,
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  '${book.author}',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: index == _index ? 12 : 8,
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
+                child: Column(children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image(
+                      height: 260,
+                      width: 180,
+                      image: AssetImage(book.image),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Text('${book.name}',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: _index == index ? 17 : 12,
+                      fontWeight: _index == index ? FontWeight.bold : FontWeight.normal,
+                    ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Text('${book.author}',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: _index == index ? 13 : 10,
+                        fontWeight: _index == index ? FontWeight.bold : FontWeight.normal,
+                      ),),
+                  )
+                ],),
               ),
             ),
-          ],
-        ),
+          );
+        },
       ),
+    )
     );
   }
 }
